21ff13067d446624eadaf17c0fa37c7205c7cd38
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-11-23 11:08:51 +0530

    Add files via upload

---
+++ C13_SAA1_Template.py
@@ -0,0 +1,8 @@
+# Define a funtion "simple_interest" to calculate simple interest given principal amount(p), rate of interest(r) and time(t)
+# SI = (P*T*R)/100
+
+
+
+
+
+# Call the function "simple_interest()" and print the result
